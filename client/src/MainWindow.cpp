897e477794d1de818d8073b82940a8c5c115ed1b
name1e5s <name1e5s@qq.com>
2019-03-09 21:32:34 +0800

    MainWindow: Always in center

--- client/src/MainWindow.cpp
+++ client/src/MainWindow.cpp
@@ -1,9 +1,15 @@
 #include "MainWindow.h"
+#include <QDesktopWidget>
+#include <QStyle>
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+  setFixedSize(this->width(), this->height());
+  setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter,
+                                  this->size(),
+                                  qApp->desktop()->availableGeometry()));
 }
 
 MainWindow::~MainWindow() {
